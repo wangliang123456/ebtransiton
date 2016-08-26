7db8b02f164c035a5cd5420f1008d6e932929511
wangliang123456 <eric@admindeMacBook-Pro.local>
2016-08-26 17:36:08 +0800

    add

--- EBInteractiveTransition/EBMapTransitionAnimator.m
+++ EBInteractiveTransition/EBMapTransitionAnimator.m
@@ -10,11 +10,33 @@
 
 @implementation EBMapTransitionAnimator
 - (NSTimeInterval)transitionDuration:(nullable id <UIViewControllerContextTransitioning>)transitionContext {
-    return 5.0;
+    return 2.0;
 }
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
-    NSLog(@"animateTransition");
+    
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIView *containerView = transitionContext.containerView;
+    UIView *fromView;
+    UIView *toView;
+    if ([transitionContext respondsToSelector:@selector(viewForKey:)]) {
+        fromView = [transitionContext viewForKey:UITransitionContextFromViewKey];
+        toView = [transitionContext viewForKey:UITransitionContextToViewKey];
+    } else {
+        fromView = fromViewController.view;
+        toView = toViewController.view;
+    }
+    BOOL isPresenting = (toViewController.presentingViewController == fromViewController);
+    [containerView addSubview:toView];
+    toView.backgroundColor = [UIColor purpleColor];
+    toView.alpha = 0.0;
+    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+        toView.backgroundColor = [UIColor greenColor];
+        toView.alpha = 1.0;
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+    }];
 }
 
 -(instancetype) initWith:(UIGestureRecognizer *) gesture {
